65bac9dd737a46352a6c284f9d94036399bec58f
willyag98 <willy.aguilar339@gmail.com>
2026-04-02 10:37:40 -0300

    Agregando archivos de objetos

---
+++ objetopers.js
@@ -0,0 +1,21 @@
+const yo = {
+ nombre: "Williams",
+ apellido: "Aguilar",
+ edad: 27,
+ añoDeNacimiento: 1998   
+};
+
+const otraPersona = {
+ nombre: "Alejo",
+ apellido: "Dilda",
+ edad: 25,
+ añoDeNacimiento: 2000    
+};
+
+console.log("Datos de:", yo.nombre);
+console.log(typeof yo.nombre)
+console.log(yo);
+
+console.log("Datos de:", otraPersona.nombre);
+console.log(typeof otraPersona.nombre);
+console.log(otraPersona);
---
+++ objetoterm.js
@@ -0,0 +1,11 @@
+const tituloTerminator2 = "Terminator 2: Judgment Day";
+const añoDeEstrenoTerminator2 = 1991;
+const duracionMinutosTerminator2 = 137;
+
+const terminator = {
+  titulo: tituloTerminator2,
+  estreno: añoDeEstrenoTerminator2,
+  duracionMinutos: duracionMinutosTerminator2
+};
+
+console.log(terminator);
